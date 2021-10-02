3af2f7d833e3dbcea6bb51d4b03f67b67d027462
arifintajul4 <arifintajul4@gmail.com>
2021-10-02 11:27:08 +0700

    add title and footer

---
+++ src/app/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return(
+    <div style={{textAlign:'center', color:'gray'}}>
+      <p>Made with &hearts; by BayuIk</p>
+    </div>
+  );
+};
+
+export default Footer;
